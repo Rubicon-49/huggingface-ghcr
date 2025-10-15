910eefc8b2676853aa16a861f5986965da929eed
Martin Diergardt <m.rubicon.49@gmail.com>
2025-10-15 23:17:25 +0200

    added icons for index.html and upated app.py

--- src/app.py
+++ src/app.py
@@ -11,12 +11,19 @@ Usage:
     uvicorn app:app --reload
 Then visit: http://127.0.0.1:8000
 """
-
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from logging_config import setup_logging
 from routes.translate import router as translate_router
 
 setup_logging()
 
 app = FastAPI(title="Multi-lingual Translation App")
+
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent / "static"),
+    name="static",
+)
 app.include_router(translate_router)
